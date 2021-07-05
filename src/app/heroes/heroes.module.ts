cb2336e9d980bcae485ec6b81ec26b2aea819191
Sergio <perdurus@gmail.com>
2021-07-05 19:44:47 +0200

    primera subida

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HeroeComponent,
+    ListadoComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
